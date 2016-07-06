b01ca41a356ea8b255aa59ebc79cf8e5305c846b
susahe <sumudu.susahe@gmail.com>
2016-07-06 06:24:47 +0530

    enrol app created

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='fogsis',
+    version='',
+    packages=['course', 'course.migrations', 'fogsis'],
+    url='',
+    license='',
+    author='sumudu',
+    author_email='',
+    description=''
+)
