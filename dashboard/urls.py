248579bb965793d1d1e5a4e9c7429f2c9f51e760
susahe <sumudu.susahe@gmail.com>
2016-07-22 20:22:20 +0530

    Dashboard Basic Setup is OK adn Index file and urls set and one view created Done

---
+++ dashboard/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'dashboard'
+
+urlpatterns = [
+
+    # url(r'^$', views.index , name = 'index'),
+    url(r'^$', views.HomePageView.as_view(), name='home'),
+    # url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='details'),
+    # # url(r'^(?P<course_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    # url(r'^course/add/$', views.CourseCreate.as_view(), name='course-add'),
+    # url(r'^module/add/$', views.ModuleCreate.as_view(), name='module-add'),
+    # url(r'^course/(?P<pk>[0-9]+)/update/$', views.CourseUpdate.as_view(), name='course-update'),
+    # url(r'^course/(?P<pk>[0-9]+)/delete/$', views.CourseDelete.as_view(), name='course-delete'),
+    # url(r'^register/$', views.UserFormView.as_view(), name='register'),
+    url(r'^accounts/login/$', views.LoginView.as_view(), name='login'),
+    #  url(r'^accounts/logout/$', views.LogOutView.as_view(), name='logout'),
+
+]
--- fogsis/urls.py
+++ fogsis/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import url, patterns
+from django.conf.urls import url, patterns, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name="base.html")),
+    url(r'^dashboard/', include('dashboard.urls')),
 ]
 
 if settings.DEBUG:
