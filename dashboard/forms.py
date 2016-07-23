276e769b6bc9802aba4a3b0149b80cfb55a0d56e
susahe <sumudu.susahe@gmail.com>
2016-07-24 04:59:59 +0530

    Add Forms update settings py  add jquery.mins.js file add dashboard login html file changes made to base.html

---
+++ dashboard/forms.py
@@ -0,0 +1,27 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, ButtonHolder, Submit
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            'username',
+            'password',
+            ButtonHolder(
+                Submit('login', 'Login', css_class='btn-primary')
+            )
+        )
